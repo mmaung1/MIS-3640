4b2c4255c264096ed3400fc44fe99e418654d748
Maung <mmaung1@babson.edu>
2019-09-10 15:03:44 -0400

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,11 @@
+print("How many seconds are there in 42 minutes 42 seconds")
+print(42*60+42)
+
+print("How many miles are there in 10 kilometers?")
+print(10/1.61)
+
+print("If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds?") 
+print((10/1.61)/2562)
+
+print("What is your average speed in miles per hour?")
+print(2562/(10/1.61))

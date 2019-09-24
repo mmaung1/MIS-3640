76e5270b81a3253f61ee866f9be8943817835aa9
mmaung1 <55001056+mmaung1@users.noreply.github.com>
2019-09-23 20:21:15 -0400

    finally finished the extra picture

--- session05/exercise1.py
+++ session05/exercise1.py
@@ -3,33 +3,38 @@ import math  #importing the math package in which we get pi from
 jonathan = turtle.Turtle() #naming the turtle function as jonathan 
 
 print(jonathan) #printing the window
-def polygon(t, n, length): 
+def polygon(turtle, n, length): 
     angle = 360 / n
     for i in range(n):
-        t.fd(length)
-        t.lt(angle)
+        turtle.fd(length)
+        turtle.lt(angle)
 
-def circle(t, r):
+def circle(turtle, r):
     circumference = 2 * math.pi * r
     n = 100
     length = circumference / n
-    polygon(t, n, length)
+    polygon(turtle, n, length)
 
-def arc(t, r, angle):
+def arc(turtle, r, angle):
     arc_length = 2 * math.pi * r * angle / 360
     n = int(arc_length / 3) + 1
     step_length = arc_length / n
     step_angle = angle / n
     for i in range(n):
-        t.fd(step_length)
-        t.lt(step_angle)
+        turtle.fd(step_length)
+        turtle.lt(step_angle)
 
-def move(t,x,y):
-    t.pu()
-    t.setpos(x,y)
-    t.pd()
+def triangle (turtle, length, angle):
+    for i in range(3):
+        turtle.forward(length)
+        turtle.left(angle)
 
-#drawing the flower inside the circle 
+def move(turtle,x,y):
+    turtle.pu()
+    turtle.setpos(x,y)
+    turtle.pd()
+
+# drawing the flower inside the circle 
 circle(jonathan, 100) #draw the circle
 jonathan.left(60)
 arc(jonathan,100,120)
@@ -56,6 +61,32 @@ arc(jonathan,100,120)
 # circle(jonathan,15)
 # move(jonathan,0,135)
 # circle(jonathan,15)
+
+#drawing the pizza 
+# circle(jonathan, 100) #draw the circle
+# move(jonathan,54,185)
+# jonathan.left(180)
+# triangle(jonathan,100,120)
+# move(jonathan,-46,12)
+# jonathan.right(180)
+# triangle(jonathan,100,120)
+# move(jonathan,90,48)
+# jonathan.left(90)
+# triangle(jonathan,100,120)
+# move(jonathan,2,99)
+# jonathan.left(60)
+# triangle(jonathan,100,120)
+# move(jonathan,76,122)
+# circle(jonathan,29)
+# move(jonathan,-40,123)
+# circle(jonathan,29)
+# move(jonathan,20,66)
+# circle(jonathan,29)
+# move(jonathan,20,180)
+# circle(jonathan,29)
+
+
+
 
 turtle.mainloop() #keeps screen open, but must be at very end
 

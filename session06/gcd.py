791dd29d7c787fc3ffb7d549edcc5bd1b6b27edf
mmaung1 <55001056+mmaung1@users.noreply.github.com>
2019-09-25 21:55:14 -0400

    Update gcd.py

--- session06/gcd.py
+++ session06/gcd.py
@@ -1,7 +1,11 @@
 def gcd(a,b):
     if b == 0:
         return a
+    elif a == 0:
+        return b
+    elif a > b:
+        return gcd(b, a%b)
     else:
-        return gcd(b, a % b) # % is a modulus returns remainder after dividin a and b, will have same sign as b rather than a.
+        return gcd(a, b%a) # % is a modulus returns remainder after dividin a and b, will have same sign as b rather than a.
 
-print(gcd(2,12))
+print(gcd(24,3))

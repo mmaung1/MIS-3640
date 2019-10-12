4308f5205dacd3c25be82a5f8d1e8a6cf289265f
mmaung1 <55122353+mmaung1@users.noreply.github.com>
2019-10-11 21:34:58 -0400

    Update hw.py

--- session11/hw.py
+++ session11/hw.py
@@ -1,17 +1,20 @@
 f = open('session09/words.txt')
 def word_reader():
-    dictionario= dict{}
+    dictionario= dict()
     for line in f: 
         words=line.strip()
         dictionario[words]=words
     return dictionario
 
+print(word_reader())
 
 def has_duplicates(f):
-    dictionario2 = dict{}
+    dictionario2 = {}
     for listings in f: 
         dictionario2{listings} = 1 + dictionario2.get(listings,0)
         if  dictionario2[listings] > 1:
             return True
     return False 
+
+print(has_duplicates(f))
 
